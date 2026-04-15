c684e6ee08119a38c7d01c9481232b217582ace9
CaptainIgelo <109104565+CaptainIgelo@users.noreply.github.com>
2026-04-15 07:44:09 +0000

    Add typed text effect with blinked cursor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// 
+// highlight for nav links on scrolled sections 
 const sections = document.querySelectorAll('section[id]');
 const navLinks = document.querySelectorAll('.nav-links a');
 
@@ -20,3 +20,49 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 sections.forEach(section => observer.observe(section));
+
+
+
+// typing effect in Herosection
+
+const typedTarget = document.querySelector('.hero h2');
+const phrases = [
+    'Technisch erfahren, lernbereit, offen', 
+    'Praktikum gesucht im großraum Köln', 
+];
+
+
+let phraseIndex = 0; 
+let charIndex = 0;
+let isDeleting = false; 
+
+
+const type = () => {
+    const currentPhrase = phrases[phraseIndex];
+
+    if (isDeleting) {
+        typedTarget.textContent = currentPhrase.slice(0, charIndex -1);
+        charIndex--; 
+    } else {
+        typedTarget.textContent = currentPhrase.slice(0, charIndex + 1);
+        charIndex ++;
+    }
+
+
+    if (!isDeleting && charIndex === currentPhrase.length) {
+        setTimeout(() => {isDeleting = true; type();}, 2000);
+        return;
+    }
+
+
+    if (isDeleting && charIndex === 0) {
+        isDeleting = false; 
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+    }
+
+    const speed = isDeleting ? 40 : 80; 
+    setTimeout(type, speed); 
+
+};
+
+type(); 
